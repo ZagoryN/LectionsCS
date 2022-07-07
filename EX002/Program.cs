cc093818eddbc96a52ebbd06ad9a0a8a90544eeb
Nina <nzagorevskaia@gmail.com>
2022-07-08 01:45:08 +0300

    Add EX002 Hello User

--- EX002/Program.cs
+++ EX002/Program.cs
@@ -1,2 +1,5 @@
 ﻿// Далее идет комментарий: See https://aka.ms/new-console-template for more information
-Console.WriteLine("1122 Hello, World!");
+Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.WriteLine(username);
