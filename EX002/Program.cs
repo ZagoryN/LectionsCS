53552aae604e04fd3094cf26c54d76dab5778f5e
Nina <nzagorevskaia@gmail.com>
2022-07-08 01:24:41 +0300

    Add EX001 Hello world project

--- EX002/Program.cs
+++ EX002/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Далее идет комментарий: See https://aka.ms/new-console-template for more information
 Console.WriteLine("1122 Hello, World!");
