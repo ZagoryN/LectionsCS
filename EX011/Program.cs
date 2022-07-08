523a7171e3e8899888e6ed888490312da18628d8
Nina <nzagorevskaia@gmail.com>
2022-07-08 22:08:03 +0300

    Add EX011

---
+++ EX011/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");  // приглашение ко вводу
+int N = Convert.ToInt32(Console.ReadLine()); // вводим число N
+int a = -N;
+while (a < N+1)
+{
+    Console.Write(a); 
+    a = a+1; 
+}
