026771d1e2ccaf89afb513ed90a1da6b3651394c
Nina <nzagorevskaia@gmail.com>
2022-07-12 20:50:06 +0300

    add EX018_3Digits_Number div 100, div 10

---
+++ EX018_3Digits_Number/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+int number = new Random().Next(100,1000);
+
+Console.WriteLine($"Наше число = {number}");
+
+int firstDigit = number / 100;  // переменная для посл.цифры
+int secondDigit = number % 10;
+
+Console.WriteLine($"первая цифра = {firstDigit}, вторая цифра = {secondDigit}, наше новое число {firstDigit}{secondDigit}");
+
+
