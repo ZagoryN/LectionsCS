e334805ac560c829739233a8e098b39c618b86fe
Nina <nzagorevskaia@gmail.com>
2022-07-12 20:28:34 +0300

    add EX017_ArrayOf2- random number, find max of digits

---
+++ EX017_ArrayOf2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите, программу, которая выводит случайное число из отрезка [10,99]
+// и показывает наибольшую цифру числа
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+int number = new Random().Next(10,100);
+
+Console.WriteLine($"Наше число = {number}");
+
+int secondDigit = number % 10;  // переменная для посл.цифры
+int firstDigit = number / 10;
+
+Console.WriteLine($"первая цифра = {firstDigit}, вторая цифра = {secondDigit}");
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"первая цифра числа больше - это {firstDigit}");
+}
+else
+{
+    Console.WriteLine($"вторая цифра числа больше - это {secondDigit}");
+}
