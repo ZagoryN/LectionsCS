9d4dae27cb8a09bd41e7711b6af510b1172ee670
Nina <nzagorevskaia@gmail.com>
2022-07-12 09:33:14 +0300

    edit commit EX014_IntroArray

--- EX014_IntroArray/Program.cs
+++ EX014_IntroArray/Program.cs
@@ -4,12 +4,12 @@ int Max(int arg1, int arg2, int arg3)
     int result = arg1;
     if (arg2 > result) result = arg2;
     if (arg3 > result) result = arg3;
-    return result;
+    return result; 
 }
 //array[0]=1
 int[] array = {11,21,31,41,15,61,17,18,191};
 //array[0] = 12; //присваивали другое значение элементу массива
-//Console.WriteLine(array[0]);
+//Console.WriteLine(array[0]); 
 
 int result = Max(
     Max(array[0],array[1],array[2]),
