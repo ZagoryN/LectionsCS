5e2e8d926587a29bbbdd4851a9d5463a291db609
Nina <nzagorevskaia@gmail.com>
2022-07-23 20:44:02 +0300

    Edit EX029_ArraySort Console.Write with array[i]

---
+++ EX029_ArraySort/Program.cs
@@ -0,0 +1,35 @@
+﻿// Упорядочивание массива
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)  // метод печати массива
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)      //метод упорядочивания массива
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
