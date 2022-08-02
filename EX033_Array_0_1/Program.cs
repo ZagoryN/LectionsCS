564857860c626417272d0b36e683914729b39306
Nina <nzagorevskaia@gmail.com>
2022-08-02 21:10:19 +0300

    Add EX033 Fill array with 0 and 1

---
+++ EX033_Array_0_1/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]
+/*void FillArray(int[] collection)
+{
+    int length = 8 ;//collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0,2);
+        index++; //index = index + 1;
+    }
+
+}
+
+void PrintArray(int[] col) //будем печатать массив
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
+*/
+
+int[] arr = new int[8];
+Random rand=new Random();
+for (int i = 0; i<arr.Length; i++)
+{
+    arr[i] = new Random().Next(0,2);
+}
+Console.Write(arr);
