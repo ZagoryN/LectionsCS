afd8ce95d0e1293050a6fa0229fd507fe0c26406
Nina <nzagorevskaia@gmail.com>
2022-08-02 20:51:36 +0300

    Add EX031 with string

--- EX031_Sum_of_digits/Program.cs
+++ EX031_Sum_of_digits/Program.cs
@@ -4,11 +4,20 @@
 89126 -> 5*/
 
 Console.Write("Введите число A: ");  
-int N = Convert.ToInt32(Console.ReadLine()); // вводим число A
+/*int N = Convert.ToInt32(Console.ReadLine()); // вводим число A
 int result = 0;
 while (N > 0)
 {
     N = N/10;
     result++;
 }
-Console.Write($"В этом числе {result} цифры");
+Console.Write($"В этом числе {result} цифры");*/
+
+//второе решение
+/*string? A = Console.ReadLine();
+
+Console.WriteLine(A.Length);*/
+
+//третье решение с TryParse
+Console.WriteLine(int.TryParse(Console.ReadLine(), out int a)); //проверка на число
+Console.WriteLine(a);
