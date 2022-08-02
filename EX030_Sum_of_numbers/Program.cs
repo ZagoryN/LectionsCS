44e9990e2e062ecce563e732a7203287621c69b7
Nina <nzagorevskaia@gmail.com>
2022-08-02 20:16:21 +0300

    Add EX030 Sum of numbers from 1 to A

---
+++ EX030_Sum_of_numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36*/
+
+Console.Write("Введите число A: ");  
+int A = Convert.ToInt32(Console.ReadLine()); // вводим число A
+int sum = 0;
+for (int i = 1; i < A+1; i++)
+    {
+        sum = sum + i;
+    }
+Console.Write(sum);
