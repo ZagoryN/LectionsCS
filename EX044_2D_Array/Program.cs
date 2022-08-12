ebc7c552f567288823022dc158b960b532ef94fa
Nina <nzagorevskaia@gmail.com>
2022-08-12 20:30:41 +0300

    Add EX044_2D_Array Print 2DArray and FillArray

---
+++ EX044_2D_Array/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+Console.WriteLine("Введите число строк m: ");  
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов n: ");  
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = FillMatrixRandomNumbers(m,n);
+PrintMatrix(matrix);
+int[,] FillMatrixRandomNumbers(int rows, int columns, int leftRange = 0, int rightRange = 10)
+// leftRange = 0 и rightRange = 10  - значения по умолчанию
+{
+    int[,] matr = new int[rows,columns];
+    Random rand = new Random();
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = rand.Next(leftRange, rightRange+1);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matrixx)
+{
+    for (int i = 0; i < matrixx.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixx.GetLength(1); j++)
+        {
+            Console.Write(matrixx[i,j]+ " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//Console.WriteLine(string.Join(", ", matrix));
