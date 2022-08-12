5018796a16baf9f32ea92482933cd850888aa90f
Nina <nzagorevskaia@gmail.com>
2022-08-12 04:28:22 +0300

    Add EX042_Fibonacci_without_Recurtion - Cycle

---
+++ EX042_Fibonacci_without_Recurtion/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+//Если N = 5 -> 0 1 1 2 3
+//Если N = 3 -> 0 1 1
+//Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int n1 = 0;
+Console.Write(n1 + " ");
+int n2 = 1;
+Console.Write(n2 + " ");
+
+for (int i = 2; i < number; i++)
+{
+    int newNumber = n1 + n2;
+    Console.Write(newNumber + " ");
+    n1 = n2;
+    n2 = newNumber;
+}
