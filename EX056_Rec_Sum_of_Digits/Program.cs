06e525a755c6dd6fedd16964a97f28bc9043cb9b
Nina <nzagorevskaia@gmail.com>
2022-08-23 00:39:22 +0300

    Add EX056_Rec_Sum_of_Digits

---
+++ EX056_Rec_Sum_of_Digits/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+//453 -> 12
+//45 -> 9
+
+Console.Write("Введите число для поиска суммы его цифр");
+int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+int res = GetSumOfDigits(inputNumber);
+Console.WriteLine(res);
+
+int GetSumOfDigits(int number)
+{
+    if (number <= 0) return 0;
+    int digit = number % 10;
+    return digit + GetSumOfDigits(number / 10);
+}
