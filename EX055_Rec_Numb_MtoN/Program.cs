8a994c91465ffb718ecf4c26b468a7837add11c5
Nina <nzagorevskaia@gmail.com>
2022-08-23 00:32:28 +0300

    Add EX055_Rec_Numb_MtoN from M to N, from N to M

---
+++ EX055_Rec_Numb_MtoN/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 65: Задайте значения M и N. Напишите программу, которая выведет 
+//все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число М:");
+int M = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumbersMtoN(int a, int b) //a<b
+{
+    if (a > b) return;
+    Console.Write(a + " ");
+    PrintNumbersMtoN(a + 1, b); 
+}
+
+void PrintNumbersNtoM(int a, int b) //a<b
+{
+    if (a > b) return;
+    PrintNumbersNtoM(a + 1, b); 
+    Console.Write(a + " ");
+}
+
+if (M < N)  PrintNumbersMtoN(M,N);
+else    PrintNumbersNtoM(N, M);
+
+
+
+
+
