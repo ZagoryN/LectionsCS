39fa3ae8048a4b97ddc55704cdf5fffc1cb5cb93
Nina <nzagorevskaia@gmail.com>
2022-08-23 00:45:04 +0300

    Add EX057_Rec_A_exp_n Возведение числа А в степень B

---
+++ EX057_Rec_A_exp_n/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 69: Напишите программу, которая на вход принимает два числа A и B, 
+//и возводит число А в целую степень B с помощью рекурсии.
+//A = 3; B = 5 -> 243 (3⁵)
+//A = 2; B = 3 -> 8
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int PowNumber(int A, int B)
+{
+    if (B == 0) return 1;
+    return A * PowNumber(A, B - 1);
+}
+
+Console.Write(PowNumber(numberA, numberB));
